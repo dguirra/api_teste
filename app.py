e00d000421dc7a425cb8f9565ef1b1ccb93bde95
guirra <diegoguirra23@gmail.com>
2016-09-07 21:24:44 -0300

    Adicionado app

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_DB'] = 'cadastro'
+mysql = MySQL(app)
+
+@app.route("/")
+def teste():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM pessoas''')
+    rv = cur.fetchall()
+    return str(rv)
+
+@app.route("/addone/<string:insert>")
+def add(insert):
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT MAX(id) FROM pessoas''')
+    maxid = cur.fetchone()
+    cur.execute('''INSERT INTO pessoas (id, nome) VALUES (%s, %s)''', (maxid[0] + 1, insert))
+    mysql.connection.commit()
+    return "Done"
+
+if __name__ == '__main__':
+    app.run(debug=True)
