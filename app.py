e214852f7887cb4932ca49c2f773a4bba8859fdd
dguirra <diegoguirra23@gmail.com>
2016-10-07 17:57:12 -0300

    Requirements para configurações de controle de versão

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/cadastro'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+    db.init_app(app)
     return app
 
 
---
+++ manage.py
@@ -0,0 +1,16 @@
+# manage.py
+
+from flask_script import Manager
+
+import app
+
+manager = Manager(app)
+
+
+@manager.command
+def hello():
+    print "hello"
+
+
+if __name__ == "__main__":
+    manager.run()
